232084014730004c4848038d07ed8df8322a0135
Indira Priyadarshini Bobburi <31742324+IndiraBobburi@users.noreply.github.com>
2018-09-14 00:17:46 -0700

    Add routing

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 var path = require("path");
-
 app.set('views', __dirname + '/views');
 
 var index = require('./routes/index.js')
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-   //res.sendFile(path.join(app.get('views')+'/index.html'));
-   res.send('GET route on index.');
+	res.sendFile('index.html', { root: "./views" })
 });
 
 //export this router to use in our index.js

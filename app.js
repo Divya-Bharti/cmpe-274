a17e903b5089f8ab096c6e9808ad68b4cd887315
vijay8608 <vijayyadav8608@gmail.com>
2018-09-14 17:58:36 -0700

    feedback form update

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ var index = require('./app_server/routes/index.js')
 var feedback = require('./app_server/routes/feedback.js')
 var StateVsDeath = require('./app_server/routes/StateVsDeath.js')
 var StateVsPop = require('./app_server/routes/StateVsPop.js')
+var heatmap = require('./app_server/routes/heatmap.js')
 
 app.use('/', index);
 app.use('/home', index);
 app.use('/feedback', feedback);
 app.use('/StateVsDeath', StateVsDeath);
 app.use('/StateVsPop', StateVsPop);
+app.use('/heatmap', heatmap);
 app.use(express.static(__dirname + "/public"));
 app.listen(3000);
